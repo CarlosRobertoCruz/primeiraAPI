453807047eb7343a7ff52eddd053bea2b0721852
ArloSindorff <98981849+CarlosRobertoCruz@users.noreply.github.com>
2022-06-30 10:45:28 -0300

    Salvando a API

---
+++ index.js
@@ -0,0 +1,19 @@
+const chalk = require('chalk');
+const fs = require('fs')
+
+const trataErro = (erro) => {
+  throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho'));
+}
+
+async function pegaArquivo (caminhoDoArquivo) {
+  const encoding = 'utf-8';
+  try{
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(chalk.green(texto));
+  }catch(erro) {
+    trataErro(erro);
+  }
+}
+
+
+pegaArquivo('./arquivos/texto1.md');
